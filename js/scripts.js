16346c537fcdd8840624f6e36e11d28b46c6fbd9
Joshua Fairchild <joshua.fairchild429@gmail.com>
2017-02-10 07:06:18 -0800

    add bootstrap, css, separate front and back end logic and create ui for the first two conversions

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,36 @@
-//liters to gallons
-var liters = function(gallons) {
+//gallons to liters
+var literConvert = function(gallons) {
 	return gallons * 3.78541;
 }
-
-var gallons = parseInt(prompt("Please enter a volume in gallons:"));
-
-alert(liters(gallons).toFixed(2) + " Liter(s)");
-
 //cups to fluid ounces
-var fluidOunces = function(cups) {
-  return cups * 8;
+var fluidOzConvert = function(cups) {
+	return cups * 8;
 }
-
-var cups = parseInt(prompt("Please enter a volume in cups:"));
-
-alert(fluidOunces(cups).toFixed(2) + " fluid ounce(s)");
-
 //quarts to pints
-var pints = function(quarts) {
-  return quarts * 2;
+var pintConvert = function(quarts) {
+	return quarts * 2;
 }
-
-var quarts = parseInt(prompt("Please enter a volume in quarts:"));
-
-alert(pints(quarts).toFixed(2) + " pints");
-
 //pounds to grams
-var grams = function(pounds) {
-  return pounds * 453.592;
+var gramConvert = function(pounds) {
+	return pounds * 453.592;
 }
 
-var pounds = parseInt(prompt("Please enter a weight in pounds:"));
+$(function() {
+	$("#gal-convert").submit(function(event) {
+		var gallons = parseInt($("#gal").val());
+		var liters = literConvert(gallons);
 
-alert(grams(pounds).toFixed(3) + " gram(s)");
+		$("#liter-output").text(liters);
+
+		event.preventDefault();
+	});
+
+	$("#cup-convert").submit(function(event) {
+		var cups = parseInt($("#cup").val());
+		var fluidOz = fluidOzConvert(cups);
+
+		$("#floz-output").text(fluidOz);
+
+		event.preventDefault();
+	});
+});
