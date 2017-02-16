c5cec44d18b20b63874377a6544df67276009af8
Joshua Fairchild and Tyson Stuart <tyson@>
2017-02-15 20:33:48 -0800

    add the headache that is coding

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//for unit convertor below
+
 //gallons to liters
 var literConvert = function(gallons) {
 	return gallons * 3.78541;
@@ -14,12 +16,26 @@ var pintConvert = function(quarts) {
 var gramConvert = function(pounds) {
 	return pounds * 453.592;
 }
+//miles to kilometers
+var kmConvert = function(miles) {
+	return miles * 1.60934;
+}
+//apples to oranges
+var orangeConvert = function(apples) {
+	return apples * .8934;
+}
 
 $(function() {
 	$("#gal-convert").submit(function(event) {
 		var gallons = parseInt($("#gal").val());
 		var liters = literConvert(gallons);
 
+		$("#liter-result").show();
+		$("#floz-result").hide();
+		$("#pint-result").hide();
+		$("#gram-result").hide();
+		$("#km-result").hide();
+		$("#orange-result").hide();
 		$("#liter-output").text(liters);
 
 		event.preventDefault();
@@ -29,6 +45,12 @@ $(function() {
 		var cups = parseInt($("#cup").val());
 		var fluidOz = fluidOzConvert(cups);
 
+		$("#liter-result").hide();
+		$("#floz-result").show();
+		$("#pint-result").hide();
+		$("#gram-result").hide();
+		$("#km-result").hide();
+		$("#orange-result").hide();
 		$("#floz-output").text(fluidOz);
 
 		event.preventDefault();
@@ -38,6 +60,12 @@ $(function() {
 		var quarts = parseInt($("#quart").val());
 		var pint = pintConvert(quarts);
 
+		$("#liter-result").hide();
+		$("#floz-result").hide();
+		$("#pint-result").show();
+		$("#gram-result").hide();
+		$("#km-result").hide();
+		$("#orange-result").hide();
 		$("#pint-output").text(pint);
 
 		event.preventDefault();
@@ -47,8 +75,131 @@ $(function() {
 		var pounds = parseInt($("#lb").val());
 		var grams = gramConvert(pounds);
 
+		$("#liter-result").hide();
+		$("#floz-result").hide();
+		$("#pint-result").hide();
+		$("#gram-result").show();
+		$("#km-result").hide();
+		$("#orange-result").hide();
 		$("#gram-output").text(grams);
 
 		event.preventDefault();
 	});
+
+	$("#mile-convert").submit(function(event) {
+		var miles = parseInt($("#miles").val());
+		var kilometers = kmConvert(miles);
+
+		$("#liter-result").hide();
+		$("#floz-result").hide();
+		$("#pint-result").hide();
+		$("#gram-result").hide();
+		$("#km-result").show();
+		$("#orange-result").hide();
+		$("#km-output").text(kilometers);
+
+		event.preventDefault();
+	});
+
+	$("#apple-convert").submit(function(event) {
+		var apples = parseInt($("#apples").val());
+		var oranges = orangeConvert(apples);
+
+		$("#liter-result").hide();
+		$("#floz-result").hide();
+		$("#pint-result").hide();
+		$("#gram-result").hide();
+		$("#km-result").hide();
+		$("#orange-result").show();
+		$("#orange-output").text(oranges);
+
+		event.preventDefault();
+	});
+
+});
+
+//for calculator below
+
+var add = function(number1, number2){
+    return number1 + number2;
+  };
+
+  var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
+
+$(function() {
+  $("form#add").submit(function(event) {
+    var number1 = parseInt($("#no1").val());
+    var number2 = parseInt($("#no2").val());
+    var result = (add(number1, number2));
+
+    $("#result-add").text(result);
+
+    event.preventDefault();
+  });
+
+  $("form#subtract").submit(function(event) {
+    var number1 = parseInt($("#no1").val());
+    var number2 = parseInt($("#no2").val());
+    var result = (subtract(number1, number2));
+    $("#result-subtract").text(result);
+
+    event.preventDefault();
+  });
+
+  $("form#multiply").submit(function(event) {
+    var number1 = parseInt($("#no1").val());
+    var number2 = parseInt($("#no2").val());
+    var result = (multiply(number1, number2));
+    $("#result-multiply").text(result);
+
+    event.preventDefault();
+  });
+
+  $("form#divide").submit(function(event) {
+    var number1 = parseInt($("#no1").val());
+    var number2 = parseInt($("#no2").val());
+    var result = (divide(number1, number2));
+    $("#result-divide").text(result);
+
+    event.preventDefault();
+  });
+
+  $("#Addition").click(function() {
+    $("#subtract").hide();
+    $("#multiply").hide();
+    $("#divide").hide();
+    $("#add").show();
+  });
+
+  $("#Subtraction").click(function() {
+    $("#subtract").show();
+    $("#multiply").hide();
+    $("#divide").hide();
+    $("#add").hide();
+  });
+
+  $("#Multiplication").click(function() {
+    $("#subtract").hide();
+    $("#divide").hide();
+    $("#add").hide();
+    $("#multiply").show();
+  });
+
+  $("#Division").click(function() {
+    $("#subtract").hide();
+    $("#multiply").hide();
+    $("#add").hide();
+    $("#divide").show();
+  });
+
 });
